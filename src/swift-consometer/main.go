4c19f895072d1b07de794f9b831655cfaba56925
Félix Cantournet <felix.cantournet@cloudwatt.com>
2015-11-05 00:54:43 +0100

    Added Configuration parsing with viper
    Probably terribly not-idiomatic.
    
    Signed-off-by: Félix Cantournet <felix.cantournet@cloudwatt.com>

--- src/swift-consometer/main.go
+++ src/swift-consometer/main.go
@@ -4,13 +4,45 @@ import (
 	"fmt"
 	"github.com/rackspace/gophercloud"
 	"github.com/rackspace/gophercloud/openstack"
+	"github.com/spf13/viper"
 )
 
-func main() {
-	options := gophercloud.AuthOptions{
-		Username: "me",
-		APIKey:   "1234567890abcdef",
+type credentials struct {
+	KeystoneURI        string `yaml:keystone_uri`
+	SwiftConsoTenant   string `yaml:swift_conso_tenant`
+	SwiftConsoUser     string `yaml:swift_conso_user`
+	SwiftConsoPassword string `yaml:swift_conso_password`
+}
+
+func buildAuthOptions() (gophercloud.AuthOptions, error) {
+	var creds credentials
+	if err := viper.UnmarshalKey("credentials", creds); err != nil {
+		return gophercloud.AuthOptions{}, err
 	}
+
+	ao := gophercloud.AuthOptions{
+		Username:         creds.SwiftConsoUser,
+		TenantName:       creds.SwiftConsoTenant,
+		Password:         creds.SwiftConsoPassword,
+		IdentityEndpoint: creds.KeystoneURI,
+	}
+	return ao, nil
+}
+
+func main() {
+
+	viper.SetConfigName("consometer")  // name of config file (without extension)
+	viper.AddConfigPath("/etc/swift/") // path to look for the config file in
+	err := viper.ReadInConfig()        // Find and read the config file
+	if err != nil {                    // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	options, err := buildAuthOptions()
+	if err != nil {
+		panic(fmt.Errorf("Fatal failed to read credentials : %s \n", err))
+	}
+
 	client, err := openstack.AuthenticatedClient(options)
 	if err != nil {
 		fmt.Println("Everything went as planned")
