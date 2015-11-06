e1300ee5158ed1e2876652ee30f942747e57cadb
Marc Fouché <marc.fouche@cloudwatt.com>
2015-11-06 19:14:21 +0100

    Create function getTenants and flag "config"
    Use the flag -config to specify the directory of the configuration file.
    Changed the way the configuration is handled in order to test the
    function getTenants to work.
    
    TODO: Make sure the authoptions are well set.

--- src/swift-consometer/main.go
+++ src/swift-consometer/main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/rackspace/gophercloud"
 	"github.com/rackspace/gophercloud/openstack"
+	"github.com/rackspace/gophercloud/openstack/identity/v2/tenants"
+	"github.com/rackspace/gophercloud/pagination"
 	"github.com/spf13/viper"
 )
 
+/*
 type credentials struct {
-	KeystoneURI        string `yaml:keystone_uri`
-	SwiftConsoTenant   string `yaml:swift_conso_tenant`
-	SwiftConsoUser     string `yaml:swift_conso_user`
-	SwiftConsoPassword string `yaml:swift_conso_password`
+	KeystoneURI        string
+	SwiftConsoTenant   string
+	SwiftConsoUser     string
+	SwiftConsoPassword string
 }
 
 func buildAuthOptions() (gophercloud.AuthOptions, error) {
 	var creds credentials
+	fmt.Println(viper.AllSettings())
 	if err := viper.UnmarshalKey("credentials", &creds); err != nil {
-		return gophercloud.AuthOptions{}, err
+		//return gophercloud.AuthOptions{}, err
+		panic(fmt.Errorf("Unable to decode into struct, %s \n", err))
 	}
 
 	ao := gophercloud.AuthOptions{
@@ -28,27 +34,55 @@ func buildAuthOptions() (gophercloud.AuthOptions, error) {
 	}
 	return ao, nil
 }
+*/
+
+func getTenants(client *gophercloud.ServiceClient) {
+	pager := tenants.List(client, nil)
+	err := pager.EachPage(func(page pagination.Page) (bool, error) {
+		tenantList, err := tenants.ExtractTenants(page)
+
+		for _, t := range tenantList {
+			// "t" will be a tenants.Tenant
+			fmt.Println(t)
+		}
+		return true, err
+	})
+	if err != nil {
+		panic(fmt.Errorf("Error processing pager: %s \n", err))
+	}
+}
 
 func main() {
-
+	ConfigPath := flag.String("config", "./etc/swift", "Path of the configuration file directory.")
+	flag.Parse()
 	viper.SetConfigType("yaml")
-	viper.SetConfigName("consometer")  // name of config file (without extension)
-	viper.AddConfigPath("./etc/swift") // path to look for the config file in
-	err := viper.ReadInConfig()        // Find and read the config file
-	if err != nil {                    // Handle errors reading the config file
+	viper.SetConfigName("consometer") // name of config file (without extension)
+	viper.AddConfigPath(*ConfigPath)  // path to look for the config file in
+	err := viper.ReadInConfig()       // Find and read the config file
+	if err != nil {                   // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 
-	options, err := buildAuthOptions()
+	//	opts, err := buildAuthOptions()
+	opts := gophercloud.AuthOptions{
+		IdentityEndpoint: viper.GetString("credentials.keystone_uri"),
+		Username:         viper.GetString("credentials.swift_conso_user"),
+		Password:         viper.GetString("credentials.swift_conso_password"),
+		TenantName:       viper.GetString("credentials.swift_conso_tenant"),
+	}
 	if err != nil {
 		panic(fmt.Errorf("Fatal failed to read credentials : %s \n", err))
 	}
-	fmt.Println(viper.AllSettings())
 
-	client, err := openstack.AuthenticatedClient(options)
+	provider, err := openstack.AuthenticatedClient(opts)
 	if err != nil {
-		fmt.Println("Everything went as planned")
-		panic("Yolo")
+		panic(fmt.Errorf("Fatal failed te create provider file : %s \n", err))
 	}
-	fmt.Println(client.IdentityEndpoint)
+
+	idClient := openstack.NewIdentityV2(provider)
+	if err != nil {
+		panic(fmt.Errorf("Fatal failed to create identity client : %s \n", err))
+	}
+	getTenants(idClient)
+	return
 }
