3793f2b7905648a4be233a2ad6efc74c2979595c
Marc Fouché <marc.fouche@cloudwatt.com>
2015-11-12 12:26:49 +0100

    Logs are now handled with github.com/Sirupsen/logrus

--- src/swift-consometer/main.go
+++ src/swift-consometer/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/rackspace/gophercloud"
 	"github.com/rackspace/gophercloud/openstack"
 	"github.com/rackspace/gophercloud/openstack/identity/v2/tenants"
 	"github.com/rackspace/gophercloud/pagination"
 	"github.com/spf13/viper"
+	"os"
 	"strings"
 )
 
@@ -23,7 +25,7 @@ func buildAuthOptions() (gophercloud.AuthOptions, error) {
 	var creds credentials
 	if err := viper.UnmarshalKey("credentials", &creds); err != nil {
 		//return gophercloud.AuthOptions{}, err
-		panic(fmt.Errorf("Unable to decode into struct, %s \n", err))
+		log.Fatal("Unable to decode into struct, %s \n", err)
 	}
 
 	ao := gophercloud.AuthOptions{
@@ -43,11 +45,22 @@ func getTenants(client *gophercloud.ServiceClient) []tenants.Tenant {
 		tenantList, err := tenants.ExtractTenants(page)
 		list = append(tenantList)
 		if err != nil {
-			panic(fmt.Errorf("Error processing pager: %s \n", err))
+			log.Fatal("Error processing pager: %s \n", err)
 		}
 		return true, nil
 	})
 	return list
+}
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stderr instead of stdout, could also be a file.
+	log.SetOutput(os.Stderr)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
 }
 
 func main() {
@@ -58,7 +71,7 @@ func main() {
 	viper.AddConfigPath(*ConfigPath)  // path to look for the config file in
 	err := viper.ReadInConfig()       // Find and read the config file
 	if err != nil {                   // Handle errors reading the config file
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		log.Fatal("Error reading config file: %s", err)
 	}
 	regionName := viper.GetString("os_region_name")
 
@@ -69,28 +82,25 @@ func main() {
 		Password:         viper.GetString("credentials.swift_conso_password"),
 		TenantName:       viper.GetString("credentials.swift_conso_tenant"),
 	}
-	if err != nil {
-		panic(fmt.Errorf("Fatal failed to read credentials : %s \n", err))
-	}
 
 	provider, err := openstack.AuthenticatedClient(opts)
 	if err != nil {
-		panic(fmt.Errorf("Fatal failed te create provider file : %s \n", err))
+		log.Fatal("Error creating provider : %s", err)
 	}
 
 	idClient := openstack.NewIdentityV2(provider)
-	if err != nil {
-		panic(fmt.Errorf("Fatal failed to create identity client : %s \n", err))
-	}
 	tenantList := getTenants(idClient)
+
 	objectStoreURL, err := provider.EndpointLocator(gophercloud.EndpointOpts{Type: "object-store", Region: regionName, Availability: gophercloud.AvailabilityAdmin})
 	if err != nil {
-		panic(fmt.Errorf("Fatal failed to retrieve object store admin url : %s \n", err))
+		log.Fatal("Error retrieving object store admin url : %s", err)
 	}
+
 	for _, tenant := range tenantList {
 		accountsURL := strings.Join([]string{objectStoreURL, "v1/AUTH_", tenant.ID}, "")
 		resp, _ := provider.Request("GET", accountsURL, gophercloud.RequestOpts{OkCodes: []int{200}})
 		fmt.Println(resp)
 	}
+
 	return
 }
